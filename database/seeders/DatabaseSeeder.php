e4b7168fafc07e13afb425b7c3911984f350abc9
JOKO SUSANTO <anthomars.am@gmail.com>
2023-09-19 22:23:59 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,16 +23,28 @@ class DatabaseSeeder extends Seeder
             'name' => 'ADMIN',
             'email' => 'admin@gmail.com',
             'usertype' => '1',
-            'password' => bcrypt('password')
+            'password' => bcrypt('password'),
         ]);
 
         \App\Models\User::create([
             'name' => 'User 1',
             'email' => 'user1@gmail.com',
             'usertype' => '0',
-            'password' => bcrypt('password')
+            'password' => bcrypt('password'),
         ]);
 
-        \App\Models\Product::factory(5)->create();
+        \App\Models\Product::create([
+            'product_name' => 'Advan Workpro',
+            'product_descriptions' => 'Intel I5 14'' FHD IPS 8GB 256GB Win 11',
+            'photo' => 'products/workpro.jpg',
+            'price' => '5000000',
+        ]);
+
+        \App\Models\Product::create([
+            'product_name' => 'Advan Workplus',
+            'product_descriptions' => 'AMD RYZEN 5 6600H 14''',
+            'photo' => 'products/workplus.jpg',
+            'price' => '7000000',
+        ]);
     }
 }
